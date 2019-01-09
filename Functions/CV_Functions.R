afe182e4095bc770d5bf79ec20a00a226f70ae20
Jerzy Wieczorek <jawieczo@colby.edu>
2019-01-09 10:43:00 -0500

    Clean up comments and remove functions for unused main simulations from CV_Functions.R

--- Functions/CV_Functions.R
+++ Functions/CV_Functions.R
@@ -201,14 +201,13 @@ multiSim = function(nrSims = 100,
 
 
 
-# Function to evaluate the output of our FS+CV procedure
-# or of our implementation of Cai & Wang's OMP+stopping rule procedure.
+# Function to evaluate the output of our FS+CV procedure.
 # Takes modelOutput = list(betahatWithInt, modelterms),
 # trueModel = indices of nonzero coefs (where intercept's index is 0),
 # and trueBetaWithInt = c(Intercept, beta).
 # Reports whether or not true model was chosen;
 # nr of false positives and false negatives;
-# and L2 estimation error (MSE) of coefficients
+# and L2 estimation error (SSE) of *coefficients*
 evalProcedure = function(procOutput, trueModel, trueBetaWithInt){
   p = length(trueBetaWithInt) - 1
   foundTrueModel = identical(procOutput$modelterms, trueModel)
@@ -226,11 +225,10 @@ evalProcedure = function(procOutput, trueModel, trueBetaWithInt){
 # Function to run and evaluate model performance;
 # also reporting the runtime.
 # If the procedure takes extra arguments
-# (like OMP needs threshold, or CrossValFS will take nFolds),
-# DO NOT just pass them along for ... to take care of!
-# This used to work, but since 6/17/16 we changed code
-# so that you MUST specify a procedure wrapper with all options set,
-# so that it ONLY takes Y and X, no other arguments!
+# (like FS_CV_K takes nFolds),
+# you CANNOT just pass them along for ... to take care of.
+# You must specify a procedure wrapper with all options set,
+# so that it ONLY takes Y and X, no other arguments.
 runAndEvalProcedure = function(Y, X, trueModel, trueBetaWithInt,
                                procedure = NULL, runGC = FALSE){
   timing = system.time({ procOutput = doCall(procedure,
@@ -247,16 +245,6 @@ runAndEvalProcedure = function(Y, X, trueModel, trueBetaWithInt,
 
 #### FS and cross-validation functions ####
 
-# CrossValFS is now just a wrapper for FS_CV_K(), for backwards compatibility
-CrossValFS = function(Y, X, nFolds = 1, trainProportion = 0.5, nSplits = 1){
-  FS_CV_K(Y, X, nFolds, trainProportion, nSplits)
-}
-
-# Write separate functions for
-# FS+CV over K (choose model size)
-# vs
-# FS+CV over ResCor (choose threshold for corr b/w residuals and X columns)
-
 
 # Run FS path with known (true) value of K
 # (NOT estimating it with CV or otherwise);
@@ -264,10 +252,12 @@ CrossValFS = function(Y, X, nFolds = 1, trainProportion = 0.5, nSplits = 1){
 FS_KnownK = function(Y, X, K){
   # If we run this when true K > p,
   # impossible to get the right model, so don't bother
-  # (?Is this OK? Should work fine for Prob(correct selection),
-  #  but could throw off results for nr FPs or FNs;
-  #  but I suppose we'll delete any such simulation results
-  #  from reported results anyway.)
+  # (This should work fine for Prob(correct selection),
+  #  but would throw off results for nr FPs or FNs...
+  #  However, we'll delete any simulation results
+  #  from settings where K>p anyway,
+  #  since we KNOW those don't make sense to report
+  #  when our goal is to study Prob(correct selection).)
   if(K > ncol(X)){
     return(tidyEsts(c("(Intercept)" = 0), p = ncol(X)))
   }
@@ -332,95 +322,6 @@ FS_CV_K = function(Y, X, nFolds = 1, trainProportion = 0.5, nSplits = 1,
   return(tidyEsts(estcoefs, p = ncol(X)))
 }
 
-
-# Same as FS_CV_K(), but instead of using CV to choose K directly,
-# use CV to choose a threshold for the ResCorr:
-# the max absolute correlation between current model's residual and any X column.
-# Choose a threshold by CV, then go back to full model
-# and use the first model along path whose ResCorr is below that threshold.
-# (Analogous to C&W's threshold for OMP, but
-#  (1) chosen by CV instead of theory, and
-#  (2) normalized correlation, not just inner product,
-#      so it's NOT necessarily monotonic along model path...)
-# 
-# WAIT: do we really want |Corr(R_t, X_i)| ?
-# It should really be |Corr(R(Y|X_t), R(X_i|X_t))| instead.
-# Would that be monotonic instead?
-FS_CV_ResCorr = function(Y, X, nFolds = 1, trainProportion = 0.5, nSplits = 1){
-  # EVENTUALLY:
-  # Run full model path on full dataset
-  # Get the (monotonic-ized) fullResCorr path
-  # Use SS or CV to train model paths;
-  #   find and test the models along fullResCorr path;
-  #   and choose the fullResCorr that gave best (mean or modal) testRSS
-  # Return to full dataset and get the chosen ResCorr's model estcoefs
-  # Pass them to tidyEsts and return
-  
-  # BUT FOR NOW, until we get that working...
-  # just return a hardcoded null estimated model,
-  # so we can test whether interface works even if internals don't
-  tidyEsts(estcoefs = c("(Intercept)" = 0),
-           p = ncol(X))
-}
-
-
-
-# FS model chosen by algorithm at start of Miller (2002), Ch.4:
-# Generate new artificial X variables as random uniform draws.
-# (By default, make as many artificial X's as real ones.)
-# Run FS model path and stop as soon as an artifical variable is chosen;
-# select the largest model with no artificial variables.
-# (TODO: also allow "nSplits" to be used here;
-#  though instead of really being new splits,
-#  it's the number of times to generate a new artificial dataset
-#  and re-choose model size.)
-# (TODO: handle the possible edge case, if p > n,
-#  that NO fake Xs are chosen... What will which.max do then?)
-FS_M4 = function(Y, X, pNew = ncol(X), nSplits = 1) {
-  n = nrow(X); p = ncol(X)
-  fake.formula = formula(paste0("Y~",
-                                paste0(paste0("X", 1:(p + pNew)),
-                                       collapse="+")))
-  real.formula = formula(paste0("Y~",
-                                paste0(paste0("X", 1:p),
-                                       collapse="+")))
-  
-  Khats = vector("integer", length = nSplits)
-  for(ss in 1:nSplits) {
-    Xfake = normalize(matrix(runif(n * pNew), n))
-    dataset = data.frame(Y = Y, cbind(X, Xfake))
-    leaps.out = regsubsets(fake.formula, dataset, method = "forward",
-                           nvmax = min(n, p + 1)) # Needn't go more than p steps
-    # Find the first model that accepted a fake X:
-    # find the first row of summary(leaps.out)$which
-    # that has TRUE in any of the last pNew columns.
-    # Then subtract 1 to go back a step
-    anyFakes = apply(summary(leaps.out)$which[, tail(1:(1+p+pNew), pNew)],
-                     1, any)
-    if(any(anyFakes)) {
-      Khats[ss] = which.max(anyFakes) - 1
-    } else {
-      # If we never chose a fake variable (possible if p > n),
-      # use the last model in the path
-      Khats[ss] = length(anyFakes)
-    }
-  }
-  
-  Khat = Mode(Khats)
-  # Not necessarily OK to reuse last run of model:
-  # the first Khat steps *might* include fake Xs
-  # if we used multiple "splits" and the last one wasn't a winner...
-  if(nSplits > 1) {
-    dataset = data.frame(Y = Y, X)
-    leaps.out = regsubsets(real.formula, dataset, method = "forward", nvmax = Khat)
-  }
-  if(Khat == 0) {
-    estcoefs = coef(lm(Y ~ 1, dataset))
-  } else {
-    estcoefs = coef(leaps.out, id = Khat)
-  }
-  return(tidyEsts(estcoefs, p = ncol(X)))
-}
 
 
 # Common code for FS_...() functions,
@@ -551,28 +452,11 @@ trainFS = function(Y, X){
   # Run forward stepwise, from intercept-only to min(n, p) predictors
   full.formula = formula(paste0("Y~", paste0(colnames(dataset)[-1], collapse="+")))
 
-#   step.out = step(lm(Y ~ 1, data = dataset),
-#                   scope = full.formula, direction = "forward", trace = 0,
-#                   steps = min(n, p),
-#                   k = 0) # set k=0 for no penalty, so we add all possible terms
-#   # Return the sequence of model formulas,
-#   # using the ordered sequence of terms entered in step.out$anova$Step
-#   formulas = sapply(1:length(step.out$anova$Step),
-#                     function(x) paste0("Y ~ 1",
-#                                        paste0(step.out$anova$Step[1:x],
-#                                               collapse = " ")))
-#   # Compute betahatWithInt for each model in the sequence
-#   betahatsWithInt = lapply(formulas,
-#                     function(x) lm(x, dataset)$coefficients)
-  
   # Speed up by replacing step() and lm()
   # with regsubsets() from leaps package
   leaps.out = regsubsets(full.formula, dataset,
                          method = "forward", nvmax = min(n, p))
-#   idmax = ifelse(any(is.na(leaps.out$rss)),
-#                  which.min(!is.na(leaps.out$rss)),
-#                  length(leaps.out$rss) - 1)
-  
+
   # Allow the possibility that intercept-only model is best
   betahatsWithInt = list(coef(lm(Y~1)))
   # Append the other coefs from leaps.out
@@ -604,122 +488,8 @@ testFS = function(Y, X, betahatsWithInt){
 
 
 
-#### OMP functions ####
-
-# OMP function takes in (Y,X) and a stopping rule:
-# right now, stopping rule is just the CW8 rule threshold
-# (threshold is on max abs inner product
-#  between residual and normalized X columns.)
-# Produces betahatWithInt and modelterms, same as our FS functions.
-OMP = function(Y, X, threshold = NULL, eta = 1, sigma = 1) {
-  n = nrow(X); p = ncol(X)
-  
-  if(is.null(threshold)) {
-    # Are we right to include 1/sqrt(n) here?
-    # C&W didn't do that explicitly;
-    # they assumed the NORMALIZED-X model has constant noise variance,
-    # but we generate RAW-X model with constant noise variance,
-    # so scaling sigma by sqrt(n) seems appropriate here.
-    threshold = sigma*sqrt(2*(1+eta)*log(p)/n)
-  }
-  
-  # ? Does OMP require zero-mean and unit-norm Y?
-  # If we are not estimating an intercept as part of OMP,
-  # seems like we should at least de-mean Y and all the Xs.
-  # Probably no need to unit-norm Y (scale shouldn't matter there)
-  # but also shouldn't hurt.
-  Res = normalize(matrix(Y, ncol = 1))
-  # Columns of X definitely must be 0-mean, 1-norm.
-  Xnormed = normalize(X)
-  colnames(Xnormed) = paste0("X", 1:p)
-  Xout = 1:p
-  Xin = vector("integer")
-  InnerProds = abs(t(Res) %*% Xnormed)
-  
-  # Could probably be more efficient if
-  # --we drop rows of Xnormed before calc'ing InnerProds ?
-  # --we use qr() instead of resid(lm()) ?
-  while(max(InnerProds) > threshold &
-        length(Xin) < min(n, p)) {
-    best = which.max(InnerProds[])
-    Xin = c(Xin, best)
-    Xout = Xout[!Xout == best]
-    Res = resid(lm(Res ~ Xnormed[, Xin]))
-    InnerProds = abs(t(Res) %*% Xnormed)
-  }
-  
-  # Get betahatWithInt for the chosen vars,
-  # but also restructure it so it gives us ALL p+1 vars,
-  # with 0s as appropriate
-  dataset = data.frame(Y = Y, X)
-  betahatWithInt = as.matrix(dataset[1, ])
-  colnames(betahatWithInt)[1] = "(Intercept)"
-  betahatWithInt[1, ] = 0
-  omp.formula = formula(paste0("Y~", ifelse(length(Xin) == 0,
-                                            "1",
-                                            paste0("X", Xin, collapse="+"))))
-  estcoefs = coef(lm(omp.formula, dataset))
-  betahatWithInt[, names(estcoefs)] = estcoefs
-
-  modelterms = sort(Xin)
-  return(list(betahatWithInt = betahatWithInt,
-              modelterms = modelterms))
-}
-
-# Now just run OMP with known K,
-# for sake of evaluating how often it succeeds
-OMP_KnownK = function(Y, X, K) {
-  # Code copied from OMP() above;
-  # ought to refactor to avoid code copying, eventually
-  n = nrow(X); p = ncol(X)
-  dataset = data.frame(Y = Y, X)
-  betahatWithInt = as.matrix(dataset[1, ])
-  colnames(betahatWithInt)[1] = "(Intercept)"
-  betahatWithInt[1, ] = 0
-  
-  if(K == 0) {
-    estcoefs = coef(lm(Y ~ 1, dataset))
-  } else {
-    Res = normalize(matrix(Y, ncol = 1))
-    Xnormed = normalize(X)
-    colnames(Xnormed) = paste0("X", 1:p)
-    Xout = 1:p
-    Xin = vector("integer")
-    InnerProds = abs(t(Res) %*% Xnormed)
-    while(length(Xin) < K) {
-      best = which.max(InnerProds[])
-      Xin = c(Xin, best)
-      Xout = Xout[!Xout == best]
-      Res = resid(lm(Res ~ Xnormed[, Xin]))
-      InnerProds = abs(t(Res) %*% Xnormed)
-    }
-    omp.formula = formula(paste0("Y~", ifelse(length(Xin) == 0,
-                                              "1",
-                                              paste0("X", Xin, collapse="+"))))
-    estcoefs = coef(lm(omp.formula, dataset))
-  }
-  betahatWithInt[, names(estcoefs)] = estcoefs
-  modelterms = sort(Xin)
-  return(list(betahatWithInt = betahatWithInt,
-              modelterms = modelterms))
-}
-
-
-
-
-
-
-
-
 
 #### Small helper functions ####
-
-# Helper function for multiSim(),
-# reporting the standard deviation of each result row/column combo
-# over the 3rd dimension (nrSims)
-facetSDs = function(x) {
-  apply(x, 1:2, sd)
-}
 
 # L2() calculates L2 norm of a vector
 L2 = function(vec) {
@@ -739,7 +509,9 @@ normalize = function(mat) {
 # Assumes the inputs are non-negative integers; ought to write a test...
 # (R's built-in mode() is about storage type, not central tendency)
 Mode = function(vec) {
-  # which.max(tabulate(vec)) # does not work for 0 (or negative integers)
+  ## First try was...
+  # which.max(tabulate(vec)) ## does not work for 0 (or negative integers)
+  ## ...so instead, this works better:
   as.integer(names(which.max(table(vec))))
 }
 
@@ -747,15 +519,6 @@ Mode = function(vec) {
 # within each row, take sd() across columns
 rowSDs = function(mat) {
   apply(mat, 1, sd)
-}
-
-# Create strings for printing: "mean (SD)"
-# with a fixed nr of digits past the decimal
-printMeansSDs = function(mean, sd, digits = 2) {
-  paste0(formatC(mean, digits, format="f"),
-         " (",
-         formatC(sd, digits, format="f"),
-         ")")
 }
 
 # Compute MOEs: by default uses 2*SE, where SE = SD/sqrt(n),
@@ -810,7 +573,7 @@ is.wholenumber = function(x, tol = .Machine$double.eps^0.5) {
 }
 
 
-#### Functions to generate beta, Sigma, epsilon ####
+#### Functions to generate beta, Sigma, X, epsilon ####
 
 # Note again: beta refers to the vector of non-intercept parameters;
 # betaWithInt refers to that vector augmented by an intercept term
@@ -861,27 +624,6 @@ Sigma.Identity = function(p = 6){
   return(diag(p))
 }
 
-# Generate Sigma: perturbed identity,
-# with nonzero correlation in just one pair of variables.
-# type = c("withinTrue", "withinFalse", "across")
-# indicates whether the nonzero corr is
-# between two True Xs (1, 2),
-# between two False Xs (p-1, p),
-# or across True and False Xs (1, p).
-Sigma.OneBigEntry = function(p = 6, mu = 0.1,
-                             type = "withinTrue"){
-  stopifnot(0 < p)
-  stopifnot(0 < mu & mu < 1)
-  Sigma = Sigma.Identity(p)
-  entry = switch(type,
-                 withinTrue = c(1, 2),
-                 withinFalse = c(p-1, p),
-                 across = c(1, p))
-  Sigma[entry[1], entry[2]] = mu
-  Sigma[entry[2], entry[1]] = mu
-  return(Sigma)
-}
-
 # Generate Sigma: constant off-diagonal correlation,
 # ones on diagonal
 Sigma.ConstCorr = function(p = 6, mu = 0.1){
@@ -896,9 +638,9 @@ Sigma.ConstCorr = function(p = 6, mu = 0.1){
 # but negative for first K variables and positive for others;
 # ones on diagonal.
 # (If SigmaApprox=TRUE, start with this matrix and edit to be pos.definite:
-#  Jing suggested that if there are any negative eigenvalues,
+#  At first we thought that if there are any negative eigenvalues,
 #  replace them with value of smallest positive eigenvalue,
-#  and reconstruct Sigma from that -- but Jerzy discovered that
+#  and reconstruct Sigma from that -- but later discovered that
 #  Matrix::nearPD() has a more sophisticated approach built-in.)
 # https://stat.ethz.ch/R-manual/R-devel/library/Matrix/html/nearPD.html
 # https://www.rdocumentation.org/packages/sfsmisc/versions/1.1-1/topics/posdefify
@@ -927,30 +669,14 @@ Sigma.NegKPosOthers = function(p = 6, mu = 0.1, K = 5, SigmaApprox = FALSE){
     ## Not obvious whether my version or nearPD() is better generally,
     ## but at least in the special case of
     ## Sigma.NegKPosOthers(p=11, mu=5/(2*10), K=10, Approx = T)
-    ## the nearPD() version gives negative corrs for 1:K and pos for last,
+    ## the nearPD() version gives negative corrs for 1:K and pos for last
+    ## (which is what we wanted),
     ## while the naive version gives near-zero corrs everywhere.
     ## In other situations, neither naive nor nearPD() gives neg. corrs
-    ## for 1:K, and I'm not sure how to enforce them otherwise,
-    ## but I suppose this is a decent first thing to try.
+    ## for 1:K, and I'm not sure how to enforce them otherwise.
   } else {
     stopifnot(isPosDef(Sigma))
   }
-  return(Sigma)
-}
-# Also predefine a version with Approx=TRUE
-Sigma.NegKPosOthers.Approx = function(p = 6, mu = 0.1, K = 5) {
-  Sigma.NegKPosOthers(p, mu, K, SigmaApprox = TRUE)
-}
-
-
-# Generate Sigma: Toeplitz with power decay
-# (ones on diagonal, then constant off-diagonals decreasing geometrically)
-Sigma.Toeplitz = function(p = 6, mu = 0.1, fac = 1 - mu){
-  stopifnot(0 < p)
-  stopifnot(0 < mu & mu < 1)
-  stopifnot(0 < fac & fac < 1)
-  topRow = c(1, mu * fac^(0:(p-2)))
-  Sigma = toeplitz(topRow)
   return(Sigma)
 }
 
@@ -978,27 +704,6 @@ epsilon.Normal = function(n = 115, sigma = 1, rescaleByN = TRUE){
   sd = ifelse(rescaleByN, sigma / sqrt(n), sigma)
   return(rnorm(n, mean = 0, sd = sd))
 }
-
-# Generate epsilon: t with 3 df,
-# rescaled by variance of n such draws
-# (to have approximate unit L2 length on average)
-# ...unless rescaleByN = FALSE, then variance is sigma^2 (1 by default)
-epsilon.t3 = function(n = 115, sigma = 1, rescaleByN = TRUE){
-  stopifnot(1 <= n)
-  # When df > 2, variance of t is [df / (df - 2)]
-  # so here each entry's variance is 3.
-  fac = ifelse(rescaleByN, n * 3 / sigma^2, 3 / sigma^2)
-  return(rt(n, df = 3) / sqrt(fac))
-}
-
-# As above, but now NOT rescaling to unit variance,
-# just like epsilon.t2 function below
-epsilon.t3Var3 = function(n = 115, sigma = 1, rescaleByN = TRUE){
-  stopifnot(1 <= n)
-  fac = ifelse(rescaleByN, n / sigma^2, 1 / sigma^2)
-  return(rt(n, df = 3) / sqrt(fac))
-}
-
 
 
 # Generate epsilon: t with 2 df.
